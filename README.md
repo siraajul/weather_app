814442bd849216204444ec0caef6874a7a9963ae
siraajul <shirajulislamparvez@gmail.com>
2023-07-10 21:08:24 +0600

    Final Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Weather App
+
+Ostad Module 09 Assignment App
+
+## Project Demonstration
+<img src = "screenshot/mainlayout.png" width ="200" />
---
+++ android/app/src/main/kotlin/com/siraajul/weather_app/weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.siraajul.weather_app.weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
